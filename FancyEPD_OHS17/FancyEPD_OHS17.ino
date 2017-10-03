95f015670624a54a8c73f1efebed9528f75d6791
Drew Fustini <drew@pdp7.com>
2017-10-03 02:38:14 -0500

    update images for OHS17 badge

--- FancyEPD_OHS17/FancyEPD_OHS17.ino
+++ FancyEPD_OHS17/FancyEPD_OHS17.ino
@@ -10,93 +10,106 @@
 FancyEPD epd(k_epd_model_E2215CS062, 17, 16, 14, 15);	// hardware SPI
 
 void setup() {
-	bool success = epd.init();
+  bool success = epd.init();
 
-	if (!success) {
-		// Panic and freak out
-		return;
-	}
-}
+  if (!success) {
+    // Panic and freak out
+    return;
+  }
 
-void loop() {
-  
   epd.setRotation(0);
   epd.setBorderColor(0x00); // black
   epd.updateScreenWithImage( drewfustini, k_image_4bit_monochrome );
   delay(DELAY_BETWEEN_IMAGES_MS);
-  
+
+}
+
+void loop() {
+
+  epd.setRotation(2);
+  epd.setBorderColor(0x00); // black
+  epd.updateScreenWithImage( drewlandscape, k_image_4bit_monochrome );
+  delay(DELAY_BETWEEN_IMAGES_MS);
+
+  /*epd.setRotation(2);
+    epd.setBorderColor(0x00); // black
+    epd.updateScreenWithImage( angel_4bit, k_image_4bit_monochrome );
+    delay(DELAY_BETWEEN_IMAGES_MS);*/
+
   epd.setRotation(2);
   epd.setBorderColor(0x00); // black
   epd.updateScreenWithImage( ohs17, k_image_4bit_monochrome );
   delay(DELAY_BETWEEN_IMAGES_MS);
-  
+
   epd.setRotation(0);
   epd.setBorderColor(0x00); // black
   epd.updateScreenWithImage( ohs2010, k_image_4bit_monochrome );
   delay(DELAY_BETWEEN_IMAGES_MS);
-  
-	/*drawCircles();
-	drawLabel("Update:\n builtin_refresh");
-	epd.setBorderColor(0x00);	// white
-	epd.updateScreen(k_update_builtin_refresh);
-	delay(DELAY_BETWEEN_IMAGES_MS);
 
-	drawLines();
-	drawLabel("Update:\n  quick_refresh");
-	epd.updateScreen(k_update_quick_refresh);
-	delay(DELAY_BETWEEN_IMAGES_MS);
 
-	drawCircles();
-	drawLabel("Update:\n   no_blink");
-	epd.updateScreen(k_update_no_blink);
-	delay(DELAY_BETWEEN_IMAGES_MS);
 
-	drawLines();
-	drawLabel("Update:\n    partial");
-	epd.updateScreen(k_update_partial);
-	delay(DELAY_BETWEEN_IMAGES_MS);
+  /*drawCircles();
+    drawLabel("Update:\n builtin_refresh");
+    epd.setBorderColor(0x00);	// white
+    epd.updateScreen(k_update_builtin_refresh);
+    delay(DELAY_BETWEEN_IMAGES_MS);
 
-	// Angel
-	epd.setBorderColor(0xff);	// black
-	epd.updateScreenWithImage(angel_4bit, k_image_4bit_monochrome, k_update_quick_refresh);
-	delay(DELAY_BETWEEN_IMAGES_MS);
+    drawLines();
+    drawLabel("Update:\n  quick_refresh");
+    epd.updateScreen(k_update_quick_refresh);
+    delay(DELAY_BETWEEN_IMAGES_MS);
 
-	// Angel
-	epd.setBorderColor(0x00);	// white
-	epd.updateScreenWithImage(angel2_8bit, k_image_8bit_monochrome, k_update_quick_refresh);
-	delay(DELAY_BETWEEN_IMAGES_MS);
+    drawCircles();
+    drawLabel("Update:\n   no_blink");
+    epd.updateScreen(k_update_no_blink);
+    delay(DELAY_BETWEEN_IMAGES_MS);
 
-	// Doggy
-	epd.setBorderColor(0x40);	// grey-ish
-	epd.updateScreenWithImage(doggy_2bit, k_image_2bit_monochrome, k_update_quick_refresh);
-	delay(DELAY_BETWEEN_IMAGES_MS);
- */
+    drawLines();
+    drawLabel("Update:\n    partial");
+    epd.updateScreen(k_update_partial);
+    delay(DELAY_BETWEEN_IMAGES_MS);
+
+    // Angel
+    epd.setBorderColor(0xff);	// black
+    epd.updateScreenWithImage(angel_4bit, k_image_4bit_monochrome, k_update_quick_refresh);
+    delay(DELAY_BETWEEN_IMAGES_MS);
+
+    // Angel
+    epd.setBorderColor(0x00);	// white
+    epd.updateScreenWithImage(angel2_8bit, k_image_8bit_monochrome, k_update_quick_refresh);
+    delay(DELAY_BETWEEN_IMAGES_MS);
+
+    // Doggy
+    epd.setBorderColor(0x40);	// grey-ish
+    epd.updateScreenWithImage(doggy_2bit, k_image_2bit_monochrome, k_update_quick_refresh);
+    delay(DELAY_BETWEEN_IMAGES_MS);
+  */
 }
 
 void drawCircles()
 {
-	epd.clearBuffer();
-	for (uint8_t i = 0; i < 5; i++) {
-		uint8_t radius = random(1, 80);
-		epd.drawCircle(random(epd.width()), random(epd.height()), radius, 0xff);
-	}
+  epd.clearBuffer();
+  for (uint8_t i = 0; i < 5; i++) {
+    uint8_t radius = random(1, 80);
+    epd.drawCircle(random(epd.width()), random(epd.height()), radius, 0xff);
+  }
 }
 
 void drawLines()
 {
-	epd.clearBuffer();
-	for (uint8_t i = 0; i < 15; i++) {
-		epd.drawLine(random(epd.width()), random(epd.height()), random(epd.width()), random(epd.height()), 0xff);
-	}
+  epd.clearBuffer();
+  for (uint8_t i = 0; i < 15; i++) {
+    epd.drawLine(random(epd.width()), random(epd.height()), random(epd.width()), random(epd.height()), 0xff);
+  }
 }
 
 void drawLabel(String str)
 {
-	// Background box
-	const uint8_t box_height = 20;
-	epd.fillRect(0, 0, epd.width(), box_height, 0x0);
-	epd.drawFastHLine(0, box_height, epd.width(), 0xff);
+  // Background box
+  const uint8_t box_height = 20;
+  epd.fillRect(0, 0, epd.width(), box_height, 0x0);
+  epd.drawFastHLine(0, box_height, epd.width(), 0xff);
 
-	epd.setCursor(0, 0);
-	epd.print(str);
+  epd.setCursor(0, 0);
+  epd.print(str);
 }
